c8436c9d98f908c73ba1f0f5b87b85260847d624
Natalie Jane Edson <nataliejaneedson@gmail.com>
2019-12-16 19:44:29 -0800

    home page first row down

--- src/Home.elm
+++ src/Home.elm
@@ -5,8 +5,8 @@ import Html.Attributes exposing (..)
 import Browser
 import Browser.Events exposing (onResize)
 import Browser.Dom exposing (getViewport, Viewport)
-import ViewHelpers exposing (pictureLink)
-import Element as El exposing ()
+import ViewHelpers exposing (..)
+import Element as El exposing (..)
 
 
 ---- MODEL ----
@@ -45,7 +45,19 @@ update msg model =
 
 
 ---- VIEW ----
+titleText : List (El.Element msg)
+titleText = 
+    [ El.text
+        "NATALIE JANE EDSON"
+    ]
 
+subtitleText : List (El.Element msg)
+subtitleText = 
+    [ El.text
+        """is a poet, programmer, and aspiring polymath based in Portland, OR. She has a lot 
+        to say about her work—what do you want to know about?
+        """
+    ]
 
 view : Model  -> Browser.Document msg
 view model =
@@ -54,22 +66,48 @@ view model =
         device = 
             classifyDevice model.windowSize
 
-        body : El.el msg
+        body : El.Element msg
         body = 
             case (device.class, device.orientation) of 
                 (BigDesktop, _) ->
-                    El
+                    El.row [] 
+                        [ El.column [El.width (fillPortion 2)] [ none]
+                        , textColumn [El.width (fillPortion 3)] 
+                            [ paragraph titleStyle titleText
+                            , paragraph subtitleStyle subtitleText
+                            ]
+                        , El.column [El.width (fillPortion 2)] [ none]
+                        ]
+                    
                 (Desktop, _) ->
-
+                    El.row [] 
+                        [ El.column [El.width (fillPortion 1)] [ none]
+                        , textColumn [El.width (fillPortion 3)] 
+                            [ paragraph titleStyle titleText
+                            , paragraph subtitleStyle subtitleText
+                            ]
+                        , El.column [El.width (fillPortion 1)] [ none]
+                        ]
+                    
                 (_, _) ->
-
+                    El.row [] 
+                        [ El.column [El.width (fillPortion 1)] [ none]
+                        , textColumn [El.width (fillPortion 3)] 
+                            [ paragraph titleStyle titleText
+                            , paragraph subtitleStyle subtitleText
+                            ]
+                        , El.column [El.width (fillPortion 1)] [ none]
+                        ]
+                    
 
 
     in 
         { title = "NJE"
         , body =
-            El.layout 
+            [El.layout 
                 []
-        }       body
+                body
+            ]
+        }
 
 
--- src/ViewHelpers.elm
+++ src/ViewHelpers.elm
@@ -1,8 +1,9 @@
 module ViewHelpers exposing (..)
 
-import Html exposing (..)
-import Html.Attributes exposing (..)
-import Element exposing (Device, DeviceClass(..), Orientation(..))
+import Html exposing (a, img, span, div, text, Html)
+import Html.Attributes exposing (src, class, href)
+import Element as El exposing (..)
+import Element.Font as EF exposing (..)
 
 pictureLink: String -> String -> String -> String -> Html msg 
 pictureLink perRowClass linkString imgSrc text_ = 
@@ -14,6 +15,81 @@ pictureLink perRowClass linkString imgSrc text_ =
             [ href linkString  
             ] 
             [ img [src imgSrc, class "square-image"] []
-            , span [class "bottom-text"] [text text_]
+            , span [class "bottom-text"] [Html.text text_]
             ]
         ]
+
+workSans =
+    typeface "Work Sans"
+
+arial = 
+    typeface "Arial"
+
+lato = 
+    typeface "Lato"
+
+georgia = 
+    typeface "Georgia"
+
+emphasisFonts : El.Attribute msg
+emphasisFonts = 
+    EF.family 
+        [ workSans
+        , arial
+        , EF.sansSerif
+        ]
+
+mainFonts : El.Attribute msg
+mainFonts = 
+    EF.family 
+        [ lato
+        , georgia
+        , EF.serif
+        ]
+
+--to-do--Implement scaled 
+
+bodyStyle : List (El.Attribute msg)
+bodyStyle = 
+    [ mainFonts
+    ]
+
+titleStyle : List (El.Attribute msg)
+titleStyle = 
+    [ emphasisFonts
+    , center
+    , EF.size 48
+    , paddingEach {noPadding | top =60, bottom = 20}
+    ]
+
+
+subtitleStyle : List (El.Attribute msg)
+subtitleStyle = 
+    [ mainFonts
+    , center
+    , EF.size 20
+    , paddingEach {noPadding | bottom = 50}
+    ]
+
+bottomTextStyle : List (El.Attribute msg)
+bottomTextStyle = 
+    [ emphasisFonts
+    , center
+    , EF.size 32
+    ]
+
+noPadding =
+    { top = 0
+    , right = 0
+    , bottom = 0
+    , left = 0
+    }
+{--.square-image {
+    width: 90%;
+    max-width: 300px;
+    max-height: 300px;
+    margin: auto;
+    float: top;
+    display: block;
+    object-fit: contain;
+--}
